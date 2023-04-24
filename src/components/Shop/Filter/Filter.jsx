f2cfd3743564dd37418aeb00797aebf5257c4ba6
Mladen Simic <mladensimic117@yahoo.com>
2023-04-24 23:03:08 +0200

    Brand Page changes (styles)

--- src/components/Shop/Filter/Filter.jsx
+++ src/components/Shop/Filter/Filter.jsx
@@ -5,8 +5,8 @@ import s from './Filter.module.scss'
 const Filter = (props) => {
   const dispatch= useDispatch();
   const filterNames = [{id:0,name:"All",
-    count: 43},{id:1,name:"Winter",count:13},{id:2,name:"Spring",count:9 },
-    {id:3,name:"Summer",count:9},{id:4,name:"Fall",count:12}]
+    count: 43},{id:1,name:"Winter",count:14},{id:2,name:"Spring",count:10 },
+    {id:3,name:"Summer",count:11},{id:4,name:"Fall",count:13}]
     const filterNamesMap = filterNames.map((elem,key) => {
         return <div className={s.box} onClick={() => {props.setFilter([elem.id , elem.count]);dispatch(setPage(1))}} >{elem.name}</div>
     })
